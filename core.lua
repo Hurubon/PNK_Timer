a1e056d483f5eda3830936016cd81d85ce139d06
Hurubon <Huruboe@gmail.com>
2025-08-10 18:26:29 +0200

    Update LibStub, bugfix with format, refactoring.

--- core.lua
+++ core.lua
@@ -16,38 +16,40 @@ function Timer:AcquireTimer(id, duration, callback)
     assert(
         type(id) == "string" or type(id) == "number",
         ("Bad argument #2 to `AcquireTimer' (expected string or number, got %q).")
-            .format(type(id)));
+            :format(type(id)));
     assert(
         type(duration) == "number",
         ("Bad argument #3 to `AcquireTimer' (expected number, got %q).")
-            .format(type(duration)));
+            :format(type(duration)));
     assert(
         type(callback) == "function",
         ("Bad argument #4 to `AcquireTimer' (expected function, got %q)")
-            .format(type(callback)));
-    assert(
-        self.timers[id] == nil,
-        ("Attempting to acquire a timer with an id that already exists (id: %s).")
-            .format(tostring(id)));
+            :format(type(callback)));
     
-    self.timers[id] = {
-        elapsed  = 0,
-        duration = duration,
-        callback = callback,
-    };
+    if self.timers[id] == nil then
+        self.timers[id] = {
+            elapsed  = 0,
+            duration = duration,
+            callback = callback,
+        };
+        return true;
+    else
+        return false;
+    end
 end
 
 function Timer:ReleaseTimer(id)
     assert(
         type(id) == "string" or type(id) == "number",
         ("Bad argument #2 to `ReleaseTimer' (expected string or number, got %q).")
-            .format(type(id)));
-    assert(
-        self.timers[id] ~= nil,
-        ("Attempting to release a timer with an id that doesn't exist (id: %s).")
-            .format(tostring(id)));
+            :format(type(id)));
     
-    self.timers[id] = nil;
+    if self.timers[id] == nil then
+        return false;
+    else
+        self.timers[id] = nil;
+        return true;
+    end
 end
 
 --[[---------------------------------------------------------------------------
